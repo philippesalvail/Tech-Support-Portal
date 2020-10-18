03e674fe667600e1a37d332bc7eb96de952fbca9
Philppe <psalvail@assetscience.com>
2020-10-18 16:23:47 -0400

    added user to db

--- server/handlers.js
+++ server/handlers.js
@@ -11,8 +11,8 @@ const options = {
 };
 
 const getClientAccount = async (req, res) => {
-  const {emailId} = req.body;
-  console.log("emailId: ", emailId);
+  const {emailId} = req.params;
+
   try {
     const client = await MongoClient(MONGO_URI, options);
     await client.connect();
@@ -26,4 +26,26 @@ const getClientAccount = async (req, res) => {
   }
 };
 
-module.exports = {getClientAccount};
+const registerClient = async (req, res) => {
+  console.log("req in registerClient ", req.body);
+  const {loginInfo, billingInfo} = req.body;
+  let newUser = {
+    username: loginInfo.nickname,
+    loginInfo: loginInfo,
+    billingInfo: billingInfo,
+  };
+
+  try {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const database = client.db("Tech_Support");
+    const table = await database.collection("Clients").insertOne(newUser);
+    assert.equal(1, table.insertedCount);
+    res.status(201).json({status: 201, data: req.body});
+    client.close();
+  } catch (error) {
+    res.status(500).json({status: 500, data: req.body, message: error.message});
+  }
+};
+
+module.exports = {getClientAccount, registerClient};
