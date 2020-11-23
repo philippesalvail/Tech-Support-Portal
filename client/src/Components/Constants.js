41511b07edcebc2dcddb6aa388611d40df4ca79d
Philppe <psalvail@assetscience.com>
2020-11-22 19:23:38 -0500

    added ip to sidebars

--- client/src/Components/Constants.js
+++ client/src/Components/Constants.js
@@ -1,1 +1,4 @@
-export const ip = "https://backend-techsupport.herokuapp.com";
+export const originalurl = "https://backend-techsupport.herokuapp.com";
+export const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+export const ip = proxyurl + originalurl;
